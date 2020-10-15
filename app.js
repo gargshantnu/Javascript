8e602fcbbb73e1cd350867addcc0cd2e63b9ebd5
Shantnu Garg <gargshantnu@gmail.com>
2020-10-16 00:23:15 +0530

    create human class inherited from base class

--- app.js
+++ app.js
@@ -13,6 +13,11 @@
     Dino.prototype = Object.create(BaseStructure.prototype);
     Dino.prototype.constructor = Dino;
 
+    function Human(species, weight, height, facts) {
+        BaseStructure.call(this, species, weight, height, facts);
+    }
+    Human.prototype = Object.create(BaseStructure.prototype);
+    Human.prototype.constructor = Human;
 
     // Create Dino Objects
 
